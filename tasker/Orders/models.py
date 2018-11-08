3977807559de1af8468a6c58ba1974009960a742
bumbleblo <bumbleblo2013@gmail.com>
2018-11-08 00:18:27 -0200

    #3 - removing the hardcoded primary id

--- tasker/Orders/models.py
+++ tasker/Orders/models.py
@@ -5,9 +5,6 @@ class Order(models.Model):
     This model represent a order from the client.
     """
 
-    # primary key of order
-    id = models.IntegerField(primary_key=True)
-    
     # project id from extern service
     product_id = models.IntegerField()
     
