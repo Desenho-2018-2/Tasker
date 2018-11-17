afe6fc6f5655a63ae8461df5b26f56dd2c4df80b
bumbleblo <bumbleblo2013@gmail.com>
2018-11-17 17:41:45 -0200

    #9 - Add observers models and relations tables
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>
    Co-authored-by: Cleber <cjj.castro3@gmail.com>

--- tasker/Orders/models/observer.py
+++ tasker/Orders/models/observer.py
@@ -1,3 +1,5 @@
+from os.path import join
+
 from django.db import models
 from Orders.models import Order
 
@@ -5,6 +7,9 @@ class Observer(models.Model):
     ip = models.CharField(max_length=16)
     port = models.IntegerField()
     link = models.CharField(max_length=100)
+
+    def __str__(self):
+        return join(self.ip, str(self.port), self.link)
 
 class Target(models.Model):
     order = models.ForeignKey(Order, on_delete=models.DO_NOTHING)
