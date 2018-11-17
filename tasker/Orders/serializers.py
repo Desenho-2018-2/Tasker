f1e12cffa4b540577f98cc92cea74d03093fcb21
bumbleblo <bumbleblo2013@gmail.com>
2018-11-17 17:42:24 -0200

    #9 - Add serializer for observer model
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>
    Co-authored-by: Cleber <cjj.castro3@gmail.com>

--- tasker/Orders/serializers.py
+++ tasker/Orders/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 from Orders.models import Order
+from Orders.models import Observer
 
 class OrdersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
 
+class ObserverSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Observer model class
+    """
 
+    class Meta:
+        model = Observer
+        fields = '__all__'
