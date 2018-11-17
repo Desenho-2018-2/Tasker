04268308ccdcd10b3ddefead4f12136efc923301
bumbleblo <bumbleblo2013@gmail.com>
2018-11-17 11:56:25 -0200

    #9 - refactor this files

--- tasker/Orders/views/order.py
+++ tasker/Orders/views/order.py
@@ -14,7 +14,7 @@ from Orders.views.state import update_state
 class CRUDOrder(APIView):
     """
     This is the CRUD for Order models.
-    """
+    """ 
 
     def post(self, request, format=None):
         """
@@ -60,9 +60,7 @@ class CRUDOrder(APIView):
         pk = request.data['delete_order_id']
 
         order_object = get_order(pk)
-
         order_object.state = CancelState().handle()
-
         order_object.save()
 
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -75,9 +73,7 @@ class CRUDOrder(APIView):
         pk = request.data['order_id']
 
         order_object = get_order(pk)
-
         order_object.state = update_state(order_object.state)
-
         order_object.save()
 
         logging.debug("Order {} updated".format(pk))
