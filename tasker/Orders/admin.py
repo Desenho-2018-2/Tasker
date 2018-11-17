986c3a993e1cf3654a99587dc85b65a49fb22510
bumbleblo <bumbleblo2013@gmail.com>
2018-11-17 17:40:12 -0200

    #9 - Register new models in admin
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>
    Co-authored-by: Cleber <cjj.castro3@gmail.com>

--- tasker/Orders/admin.py
+++ tasker/Orders/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
-
-from .models import Order
+from .models import Order, Observer, Target
 
 admin.site.register(Order)
+admin.site.register(Observer)
+admin.site.register(Target)
