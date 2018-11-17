929887db8cfee5714242cfe2ab0670491a099d1f
bumbleblo <bumbleblo2013@gmail.com>
2018-11-17 17:39:16 -0200

    #9 - Add paths for new apps
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>
    Co-authored-by: Cleber <cjj.castro3@gmail.com>

--- tasker/Orders/urls.py
+++ tasker/Orders/urls.py
@@ -1,9 +1,7 @@
-from djangos.urls import path 
+from djangos.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-import views
-
+from Orders.views.observer import OrderObserver
 
 urlpatterns = [
-    path('insert_order/', views.insert_order),
+    path('register_observer/', OrderObserver.as_view()m name='register_observer'),
 ]
-
