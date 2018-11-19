ce3b4417869e27016743f95222ee7f86c200440b
bumbleblo <bumbleblo2013@gmail.com>
2018-11-19 02:53:53 -0200

    #9 - Set the relations in Observer serializer

--- tasker/Orders/serializers.py
+++ tasker/Orders/serializers.py
@@ -3,6 +3,10 @@ from Orders.models import Order
 from Orders.models import Observer
 
 class OrdersSerializer(serializers.ModelSerializer):
+    """
+    Serializer class for Order model
+    """
+
     class Meta:
         model = Order
         fields = '__all__'
@@ -11,6 +15,9 @@ class ObserverSerializer(serializers.ModelSerializer):
     """
     Serializer for the Observer model class
     """
+
+    orders = serializers.PrimaryKeyRelatedField(many=True,
+                                                read_only=True)
 
     class Meta:
         model = Observer
