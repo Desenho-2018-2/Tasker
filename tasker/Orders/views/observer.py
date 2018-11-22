40fe06eabc7bb9e18347cfe205c82c3892a8e588
bumbleblo <bumbleblo2013@gmail.com>
2018-11-22 21:53:28 -0200

    Removing functions not used

--- tasker/Orders/views/observer.py
+++ tasker/Orders/views/observer.py
@@ -2,7 +2,8 @@ import logging
 import json
 
 from Orders.serializers import ObserverSerializer
-from Orders.models.observer import Observer
+from Orders.models.observer import Observer as obs
+from Orders.models.order import Order
 from abc import ABC, abstractmethod
 from rest_framework import status
 from rest_framework.response import Response
@@ -14,23 +15,9 @@ class AbstractTarget(ABC):
     """
 
     @abstractmethod
-    def attach(self):
-        """
-        Register a observer
-        """
-        pass
-
-    @abstractmethod
-    def detach(self):
-        """
-        Remove a observer
-        """
-        pass
-
-    @abstractmethod
     def notify(self):
         """
-        Notify all observer registered
+        Register a observer
         """
         pass
 
@@ -57,7 +44,10 @@ class Target(AbstractTarget):
         Query all oders
         """
 
-        query = Oders.objects.select_related('Observer').filter(pk=pk)
+        order_object = Order.objects.get(pk=pk)
+        query = order_object.observer_set.all()
+
+        logging.warn("Query of Target for pk: {} is {}".format(pk, query))
 
         return query
 
@@ -67,47 +57,7 @@ class Target(AbstractTarget):
         self.__observers_pk_key = pk
 
         # list with register observers
-        self.__observer = self.__update_register(pk)
-
-    def attach(self, observer):
-        """
-        Register a observer
-        """
-
-        verify_is_not_register = observer not in self.__observer
-
-        if verify_is_not_register:
-
-            logging.debug("A observer was registered")
-
-            self.__observers.append(observer)
-
-        else:
-
-            logging.debug("A observer has not been registered")
-
-        return verify_is_not_register
-
-    def detach(self, observer):
-        """
-        Remove a observerd and return a boolean
-        if the function has success
-        """
-
-        verify_is_register = observer in self.__observers
-
-        if verify_is_register:
-
-            logging.debug("An observer was removed")
-
-            self.remove(observer)
-
-        else:
-
-            logging.debug("The observer alredy has registered")
-
-        return verify_is_register
-
+        self.__observers = self.__update_register(pk)
 
     def notify(self):
         """
@@ -134,13 +84,21 @@ class Observer(AbstractObserver):
     def update(self):
         observer = Observer.objects.get(pk=self.__model_pk)
 
-        # TODO make this post the observer link
+        # TODO Change test data to payload 
         post = requests.post(observr.__str__, data={'teste', 'teste'})
         
 class OrderObserver(APIView):
     """
     Represent a post method in another service
     """
+
+    def get(self, request, format=None):
+
+        query = obs.objects.all()
+
+        response = ObserverSerializer(query, many=True)
+
+        return Response(response.data)
 
     def post(self, request, format=None):
         """
@@ -163,58 +121,3 @@ class OrderObserver(APIView):
             logging.debug("Someone try insert a observer in database")
 
             return Response(status=status.HTTP_400_BAD_REQUEST)
-
-class OrderTarget(APIView):
-    """
-    Register, attach and detach observers
-    """
-
-    def __init__(self):
-        self.__observers = []
-
-    def notify(self):
-        """
-        Notify all observers
-        """
-
-        for observer in observers:
-            observer.update()
-
-    def post(self, observer):
-        """
-        Register a new observer
-        """
-
-        response_status = None
-
-        if observer not in self.__observers:
-            self.__observers.append(observer)
-            response_status = status.HTTP_201_CREATE
-        else:
-            response_status = status=status.HTTP_400_BAD_REQUEST
-
-        return Response(status=response_status)
-
-    def delete(self, observer):
-        """
-        Remove a observer from the observer list
-        """
-
-        message = None
-        code = None
-
-        try:
-            self.__observers.remove(observer)
-            message = "Observer removed!"
-            code = status.HTTP_200_OK
-
-            logging.info("A observer as removed")
-
-        except ValueError:
-
-            message = "Observer don't exists"
-            code = status.HTTP_400_BAD_REQUEST
-
-            logging.warn("Someone try delete a observer not registered")
-
-        return Response(message, status=code)
